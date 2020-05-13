de801d1bb6a11ae36e7ddbba6b61ecd3de59fc22
t8817 <t66@umbc.edu>
2020-05-12 22:12:12 -0400

    adding splitter and fixing Classic Tree

--- Classier_Treee.py
+++ Classier_Treee.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # _*_coding:utf-8 _*_
 #@Time    :2020/4/23 23:46
-#@Author  :Millie Zeng
+#@Author  :Millie Zeng, Tanvi
 #@FileName: Classier_Treee.py
 #@Software: VScode
 
@@ -10,16 +10,14 @@ import pandas as pd
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import warnings
-warnings.filterwarnings('ignore') #ignore those warnings
-stop_words = set(stopwords.words("english"))  #create a stop_words set
+warnings.filterwarnings('ignore')
+stop_words = set(stopwords.words("english"))  #停用词词表
 
 
 def cal_sentiment(text):
-    #NLTK word tokenizer(split text into words)
-    word_tokens = word_tokenize(text)  
-    #delete unnecessary words(stop_words)
-    word_tokens=[word for word in word_tokens if word not in stop_words]  
-    # basic classifier
+    word_tokens = word_tokenize(text)  #NLTK分词
+    word_tokens=[word for word in word_tokens if word not in stop_words]  #delete unnecessary words
+    # classification tree
     if 'worst' in word_tokens:
         sentiment='neg'
     else:
@@ -41,22 +39,27 @@ def cal_sentiment(text):
                             sentiment='neu'
     return sentiment
 
-def main():
+def getTheSentiments(i):
     result={}
     index=0
-    with open("data/data3.json", 'r') as load_f:  #load in json file
+    with open("review"+str(i)+".json", 'r') as load_f:  #load in json file
         load_dict = json.load(load_f)
         for item in load_dict:
-            text=item['text'] #select 'text' column
+            text=item['text']
             text=text.lower() #lowercase of text
             text=text.replace('\n','')  #delete \n 
-            sentiment=cal_sentiment(text)  #start classifier (calculate sentiment type)
-            print(sentiment+'\t'+text) #make table
-            item['sentiment']=sentiment #create a column 'sentiment'
-            result[index]=item #make index
+            sentiment=cal_sentiment(text)  #calculate sentiment type
+            # print(sentiment+'\t'+text)
+            item['sentiment']=sentiment
+            result[index]=item
             index+=1
-    result=pd.DataFrame(result).T #create a new table
-    result.to_excel('sentiment3.xlsx',index=None)  #save results
+    result=pd.DataFrame(result).T
+    result.to_excel('sentiment'+str(i)+'.xlsx',index=None)  #save results
 
-if __name__ == '__main__':
+def main():
+    for i in range(0, 11):
+        getTheSentiments(i)
+    print("sentiment done")
+
+if __name__ == '__main__'
     main()
---
+++ data_splitter.py
@@ -0,0 +1,37 @@
+# author: Tanu Kulkarni
+import json
+import csv
+import io
+import os
+import arff
+import pandas as pd
+
+reviewFile = open('review.json', encoding="utf8")
+
+file_num = 0
+
+def save_data(review_save_data, file_num=0):
+    file_name = 'review' + str(file_num) + '.json'
+    with open(file_name, 'w', encoding="utf-8") as review_write:
+         json.dump(review_save_data, review_write)
+
+count = 1
+review_data = list()
+for i, line in enumerate(reviewFile):
+    if(count%1000==0):
+        print(str(count) + " done")
+    count += 1
+    data = json.loads(line)
+    business_id = str(data["business_id"])
+    review_id = str(data["review_id"])
+    stars = str(data["stars"])
+    text = str(data["text"]) if data["text"] is not None else "null"
+    review_data.append([business_id, review_id, stars, text])
+    if (count % 100000 == 0):
+        # save data
+        save_data(review_data, file_num)
+        file_num += 1
+        review_data.clear()
+
+save_data(review_data, file_num)
+reviewFile.close()
